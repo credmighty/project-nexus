be6475f7618e271200a7f0d3a52e60896bc0af76
Abdullah Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2025-11-26 10:36:09 +0100

    feat: add role-based authentication for admins and users

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,4 +3,5 @@ from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('profile/', ProfileView.as_view(), name='auth_profile'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
-from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer, UserDetailSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+# from rest_framework import permissions
 
 User = get_user_model()
 
@@ -10,3 +11,11 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
