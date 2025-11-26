a4df1598b236ed2a44a43adb952708010209e4a6
Abdullah Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2025-11-26 10:23:29 +0100

    feat: add role-based authentication for admins and users

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -5,7 +5,21 @@ from django.contrib.auth.password_validation import validate_password
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-
+    password = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = User
-        fields = ("id", "username", "email", "role")
+        fields = ("id", "username", "email", "password", "role")
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validate_password)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name", "role")
+        read_only_fields = ("id", "role")
